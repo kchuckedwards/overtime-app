ef3d46afc181fbd9357c03950ea27dd175ca91c7
kchucke <kevin@kevincedwards.com>
2017-10-31 20:17:55 -0700

    Added boostrap for UI and post list to index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Peter", last_name: "Piper")
+puts "1 user has been created"
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 puts "100 posts have been created"
